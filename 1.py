de744973ee4b98d3069c9bc11c0065961d1be6da
Saitama <taf10563@icloud.com>
2018-11-29 00:12:00 +0800

    default

---
+++ 1.py
@@ -0,0 +1,38 @@
+class minus:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class digit:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class operator1:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class operator2:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class decpoint:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class number:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class term:
+    def interpret(self, context):
+        return valid, rem_context
+
+
+class expression:
+    def interpret(self, context):
+       return valid, rem_context

733e512f2e1f159e4f971f3303ac10d98e8849bb
saitama9527 <36809922+saitama9527@users.noreply.github.com>
2018-12-03 22:39:08 +0800

    fin

--- 1.py
+++ 1.py
@@ -2,8 +2,6 @@ import abc
 import re
 
 
-
-
 class non_terminal:
     def __int__(self, name, val, element, visit):
         self.name = name
@@ -14,7 +12,7 @@ class non_terminal:
     def interpret(self, context):
         pass
 
-    def set_visitor(self,*args,**kwargs):
+    def set_visitor(self, *args, **kwargs):
         pass
 
     def visit(self):
@@ -64,7 +62,7 @@ class operator1(non_terminal):
 
     def interpret(self, context):
         if context[0] == '+' or context[0] == '-':
-            #print(context[0])
+            # print(context[0])
             self.val = context[0]
             valid = 1
             rem_context = context[1:len(context)]
@@ -107,23 +105,37 @@ class number(non_terminal):
         self.name = 'number'
 
     def interpret(self, context):
+        self.element = []
+        ele = 0
         t = digit()
         av = ''
         a, b = t.interpret(context)
         if a == 1:
             av = av + t.val
+            self.element.append(t.val)
+            self.element[ele] = "\t\t digit1 : " + t.val
+            ele += 1
         while a == 1:
             t = digit()
             a, b = t.interpret(b)
             if a == 1:
                 av = av + t.val
+                self.element.append(t.val)
+                self.element[ele] = "\t\t digit2 : " + t.val
+                ele += 1
         if b is not None:
             t = decpoint()
             a, b = t.interpret(b)
             if a == 1:
                 av = av + t.val
+                self.element.append(t.val)
+                self.element[ele] = "\t\t decpoint : " + t.val
+                ele += 1
         else:
             av = av + t.val
+            self.element.append(t.val)
+            self.element[ele] = "\t\t digit4 : " + repr(t.val)
+            ele += 1
             valid = 0
             rem_context = None
             # print(av)
@@ -137,9 +149,13 @@ class number(non_terminal):
                 av = av + t.val
         valid = 1
         rem_context = b
-        #print(av)
+        # print(av)
         self.val = float(av)
+        self.val = "\tnumber : " + repr(self.val)
         return valid, rem_context
+
+    def set_visitor(self, vstr, *args, **kwargs):
+        vstr.visit(self.element, self.val)
 
 
 class term(non_terminal):
@@ -170,7 +186,7 @@ class term(non_terminal):
                 self.val = self.val / t.val
         valid = a
         rem_context = b
-        #print(self.val)
+        # print(self.val)
         return valid, rem_context
 
 
@@ -182,10 +198,14 @@ class expression(non_terminal):
         self.element = []
         # minus part
         s = 0
+        ele = 0
         t = minus()
         a, b = t.interpret(context)
         if a == 1:
             s = -1
+            self.element.append(s)
+            self.element[ele] = "\t\t minus : " + repr(s)
+            ele += 1
         else:
             s = 1
         # term part
@@ -193,6 +213,8 @@ class expression(non_terminal):
         a, b = t.interpret(b)
         self.val = s * t.val
         self.element.append(t.val)
+        self.element[ele] = "\t\t term : " + repr(t.val)
+        ele += 1
         # [operator1 term]*
 
         while a == 1 and b is not None:
@@ -204,21 +226,28 @@ class expression(non_terminal):
                 elif t.val == '-':
                     s = 2
             self.element.append(t.val)
+            self.element[ele] = "\t\t op1 : " + t.val
+            ele += 1
             t = term()
             a, b = t.interpret(b)
+            if t.val * 10 % 10 == 0:
+                if t.val / 10 <= 10:
+                    t.val = int(t.val)
             self.element.append(t.val)
+            self.element[ele] = "\t\t term : " + repr(t.val)
+            ele += 1
             if s == 1:
                 self.val = self.val + t.val
             elif s == 2:
                 self.val = self.val - t.val
         valid = 1
         rem_context = b
-        #print(+self.val)
+        self.val = "\texpression : " + repr(self.val)
+        # print(+self.val)
         return valid, rem_context
 
-    def set_visitor(self,vstr,*args,**kwargs):
-        vstr.visit(self.element,self.val)
-
+    def set_visitor(self, vstr, *args, **kwargs):
+        vstr.visit(self.element, self.val)
 
     def visit(self):
         pass
@@ -228,19 +257,19 @@ class visitor:
     def visit(part):
         pass
 
+
 class structure_visitor(visitor):
-    def visit(self,part,val):
+    def visit(self, part, val):
         print("Structure:")
-        print("		 term :",part[0]
-              ,"\n		 op1 :",part[1]
-              ,"\n		 term :",part[2]
-              ,"\n	    expression : ",val)
+        for n in part:
+            print(n)
+        print(val)
 
 
 class value_visitor(visitor):
-    def visit(self,part,val):
+    def visit(self, part, val):
         print("Value:")
-        print("	    expression : ",val)
+        print(val)
 
 
 e = expression()
@@ -249,6 +278,3 @@ e.set_visitor(structure_visitor())
 e.visit()
 e.set_visitor(value_visitor())
 e.visit()
-
-#n = number()
-#v, rc = n.interpret('383.220')
